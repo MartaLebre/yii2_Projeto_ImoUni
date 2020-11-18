feab5aa6226c8bf9d28d8e1fe287b706537abb22
MartaLebre <55530854+MartaLebre@users.noreply.github.com>
2020-11-18 16:02:35 +0000

    test

---
+++ frontend/tests/acceptance/FirstCest.php
@@ -0,0 +1,20 @@
+<?php namespace frontend\tests\acceptance;
+use frontend\tests\AcceptanceTester;
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Home');
+    }
+}
