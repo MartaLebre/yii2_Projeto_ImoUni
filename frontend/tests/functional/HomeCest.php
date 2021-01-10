128a6658bb85b679585dbc41ccef4617cdb08e47
JoaoPereira5 <55530978+JoaoPereira5@users.noreply.github.com>
2021-01-10 19:04:13 +0000

    testes

--- frontend/tests/functional/HomeCest.php
+++ frontend/tests/functional/HomeCest.php
@@ -10,8 +10,5 @@ class HomeCest
     {
         $I->amOnPage(\Yii::$app->homeUrl);
         $I->see('My Application');
-        $I->seeLink('Signup');
-        $I->click('Signup');
-        $I->see('Registar nova conta');
     }
 }
