efb706c4ff5d815c3d716c321cab0130718000ba
JoaoPereira5 <55530978+JoaoPereira5@users.noreply.github.com>
2021-02-10 16:48:07 +0000

    Update HomeCest.php

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -3,20 +3,34 @@ namespace frontend\tests\acceptance;
 
 use frontend\tests\AcceptanceTester;
 use yii\helpers\Url;
+use common\fixtures\UserFixture;
 
 class HomeCest
 {
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::className(),
+                'dataFile' => codecept_data_dir() . 'login_data.php',
+            ],
+        ];
+    }
+
     public function checkHome(AcceptanceTester $I)
     {
-        $I->amOnPage('frontend/web/');
-        $I->wait(3);
+        $I->amOnPage(Url::toRoute('/site/index'));
+        $I->see('ImoUni');
+        $I->see('Encontre a sua propriedade hoje!');
+    }
 
+    public function checkLogin(AcceptanceTester $I)
+    {
+        $I->amOnPage('/site/login');
+        $I->fillField('Nome de Utilizador', 'erau');
+        $I->fillField('Password', 'password_0');
+        $I->click('login-button');
 
-
-        $I->seeLink('Signup');
-        $I->click('Signup');
-        $I->wait(2); // wait for page to be opened
-
-        $I->see('Registar nova conta');
+        $I->see('Logout');
     }
 }
